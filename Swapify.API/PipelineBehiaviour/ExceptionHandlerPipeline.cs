7d6b44bc86714ad85ca91958a4549eff674dcc22
Sandro Kavlashvili <skavlashvili@europebet.com>
2023-12-16 18:35:17 +0400

    login register and database configs done

---
+++ Swapify.API/PipelineBehiaviour/ExceptionHandlerPipeline.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Swapify.API.PipelineBehiaviour
+{
+    public class ExceptionHandlerPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }catch(Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
